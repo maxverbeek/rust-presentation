0dd594b9c84e503353618bd7f398cee35bf85ef0
Max Verbeek <m4xv3rb33k@gmail.com>
2023-05-19 02:22:25 +0200

    feat: add some dummy routes that take inputs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{self, rejection::JsonRejection},
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
 
 /// The main function is async: this is not possible.
 /// - async functions are syntactic sugar for functions that return a future
@@ -9,7 +14,10 @@ use axum::{http::StatusCode, routing::get, Router};
 async fn main() {
     let app = Router::new()
         .route("/", get(index))
-        .route("/nono", get(forbidden));
+        .route("/nono", get(forbidden))
+        .route("/office", post(return_input))
+        .route("/office2", post(return_optional))
+        .route("/office3", post(return_error));
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
@@ -28,4 +36,70 @@ async fn index() -> String {
 
 async fn forbidden() -> (StatusCode, String) {
     (StatusCode::FORBIDDEN, "forbidden".to_owned())
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Device {
+    slack_id: String,
+    mac_addr: String,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct OfficeStatus {
+    slack_id: String,
+    at_office: bool,
+}
+
+/// - Explain some pattern matching here
+/// - extract::Json is not the same as Json (axum::extract::Json vs axum::Json)
+/// - The naming convention for this is not too relevant for this.. this is a library thing
+async fn return_input(extract::Json(device): extract::Json<Device>) -> Json<OfficeStatus> {
+    let status = OfficeStatus {
+        slack_id: device.slack_id,
+        at_office: false,
+    };
+
+    Json(status)
+}
+
+/// Option types
+async fn return_optional(payload: Option<Json<Device>>) -> Json<OfficeStatus> {
+    let status = if let Some(Json(device)) = payload {
+        OfficeStatus {
+            slack_id: device.slack_id,
+            at_office: false,
+        }
+    } else {
+        OfficeStatus {
+            slack_id: "researchableoffice".to_owned(),
+            at_office: true,
+        }
+    };
+
+    Json(status)
+}
+
+/// Error pattern matching
+async fn return_error(payload: Result<extract::Json<Device>, JsonRejection>) -> Json<OfficeStatus> {
+    let status = match payload {
+        Ok(Json(device)) => OfficeStatus {
+            slack_id: device.slack_id,
+            at_office: false,
+        },
+
+        Err(JsonRejection::JsonDataError(_)) => {
+            eprintln!("missing fields, but i decided to always return an OfficeStatus json so i can't really do anything with this :D");
+            OfficeStatus {
+                slack_id: "none".to_owned(),
+                at_office: false,
+            }
+        }
+
+        Err(_) => OfficeStatus {
+            slack_id: "none".to_owned(),
+            at_office: false,
+        },
+    };
+
+    Json(status)
 }
